601908a8bf7c05791d14ee682f8afb17c3bb6267
Filip Bienkowski <bienkowskifilip@gmail.com>
2019-04-24 14:52:11 +0200

    Implemented Card

--- src/Card.java
+++ src/Card.java
@@ -1,3 +1,39 @@
 public class Card
 {
+    private String name;
+    private int value;
+
+    public static String[] getNames()
+    {
+        return new String[] {"2", "3", "4", "5", "6", "7", "8", "9", "10", "W", "D", "K", "as"};
+    }
+
+    private void assignValue()
+    {
+        if (name.equals("W") || name.equals("D") || name.equals("K"))
+        {
+            this.value = 10;
+        }
+        else if (name.equals("as"))
+        {
+            this.value = 11;
+        }
+        else
+        {
+            String[] names = Card.getNames();
+            for (int i = 0; i < 9; i++)
+            {
+                if (names[i].equals(name))
+                {
+                    this.value = i + 2;
+                }
+            }
+        }
+    }
+
+    public Card(String name)
+    {
+        this.name = name;
+        assignValue();
+    }
 }
